0bb9487a81c7df060ca2282b33c68faf97db09e2
haphan217 <s395302@rmit.edu.vn>
2021-11-08 16:31:57 +0700

    feat: add genre modal

--- src/components/HomePage/GenresList.tsx
+++ src/components/HomePage/GenresList.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { SidebarMenu } from "@ahaui/react";
 import { useHistory } from "react-router-dom";
-
+import AddGenreModal from "components/HomePage/AddGenreModal";
 interface Props {
   isOpen: boolean;
 }
@@ -16,6 +16,7 @@ export default function GenresList({ isOpen }: Props) {
 
   return (
     <div className={`Collapse ${isOpen ? "Show " : ""}sidenav u-sizeFull md:u-size2of10`}>
+      <AddGenreModal />
       <SidebarMenu size="small" current={selectedGenres} onSelect={onSelectGenre}>
         <SidebarMenu.Item eventKey="recent">Recently added</SidebarMenu.Item>
         {[1, 2, 3, 4, 5].map((i) => (

69225e5479effeb10cb2245a536f034e041beae1
haphan217 <s395302@rmit.edu.vn>
2021-11-15 16:38:32 +0700

    test: routing

---
+++ src/__tests__/router.test.tsx
@@ -0,0 +1,56 @@
+import { render, RenderResult, screen } from "@testing-library/react";
+import HomePage from "view/HomePage";
+import App from "App";
+import ItemDetails from "view/ItemDetails";
+import Login from "view/LoginPage";
+import Signup from "view/Signup";
+import TopNav from "components/layout/TopNav";
+import { MemoryRouter } from "react-router-dom";
+import { createMemoryHistory } from "history";
+
+jest.mock("components/layout/TopNav");
+jest.mock("view/HomePage");
+jest.mock("view/ItemDetails");
+jest.mock("view/LoginPage");
+jest.mock("view/Signup");
+
+const renderApp = (entry: string): RenderResult => {
+  (TopNav as jest.Mock).mockImplementation(() => <div>TopNav</div>);
+  return render(
+    <MemoryRouter initialEntries={[entry]}>
+      <App />
+    </MemoryRouter>,
+  );
+};
+
+describe("test routing", () => {
+  test("should render home page with nav bar by default", () => {
+    (HomePage as jest.Mock).mockImplementation(() => <div>HomePage</div>);
+    renderApp("/");
+    expect(screen.getByText("HomePage"));
+  });
+
+  test("should render home page for invalid route", () => {
+    (HomePage as jest.Mock).mockImplementation(() => <div>HomePage</div>);
+    renderApp("/invalid");
+    expect(screen.getByText("HomePage"));
+  });
+
+  test("should render login page for login route", () => {
+    (Login as jest.Mock).mockImplementation(() => <div>Login Page</div>);
+    renderApp("/login");
+    expect(screen.getByText("Login Page"));
+  });
+
+  test("should render signup page for signup route", () => {
+    (Signup as jest.Mock).mockImplementation(() => <div>Signup Page</div>);
+    renderApp("/signup");
+    expect(screen.getByText("Signup Page"));
+  });
+
+  test("should render item details page for item route", () => {
+    (ItemDetails as jest.Mock).mockImplementation(() => <div>Item Details</div>);
+    renderApp("/item/1");
+    expect(screen.getByText("Item Details"));
+  });
+});
